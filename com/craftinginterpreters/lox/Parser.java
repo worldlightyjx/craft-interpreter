8a38ec34ad632363374bb27f393cae93b01bd452
worldlightyjx <worldlightyjx@qq.com>
2022-12-28 22:05:42 +0800

    Add assignment

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -67,7 +67,11 @@ public class Parser {
 
 
     private Expr expression() {
-        return equality();
+        return assignment();
+    }
+
+    private Expr assignment() {
+
     }
 
     private Expr equality() {
